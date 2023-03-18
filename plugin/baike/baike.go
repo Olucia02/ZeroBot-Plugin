85f01a49f1a49803ce95c5df22532699678efd41
lianhong <2758988938@qq.com>
2023-03-18 13:06:35 +0800

    1

--- plugin/baike/baike.go
+++ plugin/baike/baike.go
@@ -13,11 +13,11 @@ import (
 )
 
 const (
-	api = "http://ovooa.caonm.net/API/bdbk/?Msg=%v" // api地址
-
+	baidu = "http://ovooa.caonm.net/API/bdbk/?Msg=%s" // api地址
+	weiji = "https://api.a20safe.com/api.php?api=23&key=7d06a110e9e20a684e02934549db1d3d&text=%s"
 )
 
-type baka struct {
+type ba struct {
 	Code int    `json:"code"`
 	Text string `json:"text"`
 	Data struct {
@@ -27,28 +27,51 @@ type baka struct {
 		URL   string `json:"url"`
 	} `json:"data"`
 }
+type we struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data []struct {
+		Content string `json:"content"`
+	} `json:"data"`
+}
 
 func init() { // 主函数
 	en := control.Register("baike", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
-		Help: "夸克百科\n" +
-			"- 百科+[关键字]",
+		Help: "百科\n" +
+			"- 百度+[关键字]" +
+			"- 维基+[关键字]",
 	})
-	en.OnRegex(`^百科\s*(.+)$`).SetBlock(true). // 正值输入
-							Handle(func(ctx *zero.Ctx) {
-			str := ctx.State["regex_matched"].([]string)[1]
-			// es := base14.EncodeString(str)
-			es, err := web.GetData(fmt.Sprintf(api, str)) // 将网站返回结果赋值
-			if err != nil {
-				ctx.SendChain(message.Text("出现错误捏：", err))
-			}
-
-			// es1 := fmt.Sprintf("%s", es)
-			var r baka                   // r数组
-			err = json.Unmarshal(es, &r) // 填api返回结果，struct地址
-			if err != nil {
-				ctx.SendChain(message.Text("出现错误捏：", err))
-			}
+	en.OnRegex(`^百度\s*(.+)$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
+		es, err := web.GetData(fmt.Sprintf(baidu, ctx.State["regex_matched"].([]string)[1])) // 将网站返回结果赋值
+		if err != nil {
+			ctx.SendChain(message.Text("出现错误捏：", err))
+		}
+		var r ba                     // r数组
+		err = json.Unmarshal(es, &r) // 填api返回结果，struct地址
+		if err != nil {
+			ctx.SendChain(message.Text("出现错误捏：", err))
+		}
+		if r.Code == 1 {
 			ctx.SendChain(message.Text(r.Data.Info+"\n详情查看:", r.Data.URL)) // 输出提取后的结果
-		})
+		} else {
+			ctx.SendChain(message.Text(r.Text))
+		}
+	})
+	en.OnRegex(`^维基\s*(.+)$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
+		es, err := web.GetData(fmt.Sprintf(weiji, ctx.State["regex_matched"].([]string)[1])) // 将网站返回结果赋值
+		if err != nil {
+			ctx.SendChain(message.Text("出现错误捏：", err))
+		}
+		var r we                     // r数组
+		err = json.Unmarshal(es, &r) // 填api返回结果，struct地址
+		if err != nil {
+			ctx.SendChain(message.Text("出现错误捏：", err))
+		}
+		if r.Code == 0 {
+			ctx.SendChain(message.Text(r.Data[0].Content)) // 输出提取后的结果
+		} else {
+			ctx.SendChain(message.Text(r.Msg))
+		}
+	})
 }
