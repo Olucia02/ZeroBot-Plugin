9d4e6a649690f411847a3077651ce7053947683f
lianhong <2758988938@qq.com>
2023-03-18 16:44:51 +0800

    111

--- plugin/baike/baike.go
+++ plugin/baike/baike.go
@@ -4,7 +4,6 @@ package baike
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
 
 	"github.com/FloatTech/floatbox/web"
 	ctrl "github.com/FloatTech/zbpctrl"
@@ -14,38 +13,31 @@ import (
 )
 
 const (
-	baidu = "http://ovooa.caonm.net/API/bdbk/?Msg=%s" // api地址
-	weiji = "http://asoiaf.huijiwiki.com/api.php?action=query&format=json&formatversion=2&list=search&srsearch=%s&srnamespace=0&srlimit=%s"
+	baidu = "https://api.a20safe.com/api.php?api=21&key=7d06a110e9e20a684e02934549db1d3d&text=%s" // api地址
+	weiji = "https://zh.wikipedia.org/w/api.php?action=query&prop=extracts&titles=%s&format=json&exintro=1"
 )
 
 type ba struct {
 	Code int    `json:"code"`
-	Text string `json:"text"`
-	Data struct {
-		Msg   string `json:"Msg"`
-		Info  string `json:"info"`
-		Image string `json:"image"`
-		URL   string `json:"url"`
+	Msg  string `json:"msg"`
+	Data []struct {
+		Content string `json:"content"`
 	} `json:"data"`
 }
 type we struct {
-	Batchcomplete bool `json:"batchcomplete"`
-	Continue      struct {
-		Sroffset int    `json:"sroffset"`
-		Continue string `json:"continue"`
-	} `json:"continue"`
+	Batchcomplete string `json:"batchcomplete"`
+	Warnings      struct {
+		Extracts struct {
+			NAMING_FAILED string `json:"*"`
+		} `json:"extracts"`
+	} `json:"warnings"`
 	Query struct {
-		Searchinfo struct {
-			Totalhits int `json:"totalhits"`
-		} `json:"searchinfo"`
-		Search []struct {
-			Ns        int    `json:"ns"`
-			Title     string `json:"title"`
-			Pageid    int    `json:"pageid"`
-			Size      int    `json:"size"`
-			Wordcount int    `json:"wordcount"`
-			Snippet   string `json:"snippet"`
-		} `json:"search"`
+		Pages map[string]struct {
+			Pageid  int    `json:"pageid"`
+			Ns      int    `json:"ns"`
+			Title   string `json:"title"`
+			Extract string `json:"extract"`
+		} `json:"pages"`
 	} `json:"query"`
 }
 
@@ -66,18 +58,14 @@ func init() { // 主函数
 		if err != nil {
 			ctx.SendChain(message.Text("出现错误捏：", err))
 		}
-		if r.Code == 1 {
-			ctx.SendChain(message.Text(r.Data.Info+"\n详情查看:", r.Data.URL)) // 输出提取后的结果
+		if r.Code == 0 {
+			ctx.SendChain(message.Text(r.Data[0].Content)) // 输出提取后的结果
 		} else {
-			ctx.SendChain(message.Text(r.Text))
+			ctx.SendChain(message.Text("百度百科未找到信息"))
 		}
 	})
-	en.OnRegex(`^维基\s*(\S+)\s*(\d+)?$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
-		fig := ctx.State["regex_matched"].([]string)[2]
-		if fig == "" {
-			fig = "1"
-		}
-		es, err := web.GetData(fmt.Sprintf(weiji, ctx.State["regex_matched"].([]string)[1], fig)) // 将网站返回结果赋值
+	en.OnRegex(`^维基\s*(.+)$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
+		es, err := web.GetData(fmt.Sprintf(weiji, ctx.State["regex_matched"].([]string)[1])) // 将网站返回结果赋值
 		if err != nil {
 			ctx.SendChain(message.Text("出现错误捏：", err))
 		}
@@ -86,16 +74,12 @@ func init() { // 主函数
 		if err != nil {
 			ctx.SendChain(message.Text("出现错误捏：", err))
 		}
-		if len(r.Query.Search) > 0 {
-			ctx.SendChain(message.Text(r.Query.Search[func(fig string) int {
-				f, _ := strconv.Atoi(fig)
-				return f
-			}(fig)].Title, "\n", r.Query.Search[func(fig string) int {
-				f, _ := strconv.Atoi(fig)
-				return f
-			}(fig)].Snippet)) // 输出提取后的结果
-		} else {
-			ctx.SendChain(message.Text("维基百科未找到信息"))
+		for k, v := range r.Query.Pages {
+			if k != "-1" {
+				ctx.SendChain(message.Text(v.Extract)) // 输出提取后的结果
+			} else {
+				ctx.SendChain(message.Text("维基百科未找到信息"))
+			}
 		}
 	})
 }
